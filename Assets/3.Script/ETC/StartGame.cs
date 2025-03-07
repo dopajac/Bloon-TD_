f008c747a5201318670077472c47096ceca502a6
dopajac <154491661+dopajac@users.noreply.github.com>
2025-03-07 13:12:19 +0900

    마무리 작업 필요

---
+++ Assets/3.Script/ETC/StartGame.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class StartGame : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        InintroScene();
+    }
+    private void InintroScene()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            SceneManager.LoadScene("InGameScene");
+        }
+    }
+}
